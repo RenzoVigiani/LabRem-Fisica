e8633199a86eaa8ac0e85dbb5535b2a02cd6dd66
maxiexe97 <maxiilopez97gmail.com>
2022-06-03 16:13:15 -0300

    Control de protocolo con nueva librería

--- Control_protocolo/Control_protocolo.ino
+++ Control_protocolo/Control_protocolo.ino
@@ -1,168 +1,91 @@
-#include <ArduinoJson.h>
-#include <UIPEthernet.h>
-#include <Servo.h>
+#include <EtherCard.h>
 
-#define const_status 150
-#define const_instruc 130
+static byte mymac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+static byte myip[] = {172,20,5,140};
+byte Ethernet::buffer[700];
+int bandera=0;
 
-EthernetServer server = EthernetServer(22);
-  //////// VAriables de json //////////////
-  // Estado
-  int num_Lab=0;
-  bool subLab=true;
-  bool iniLab=true;
-  // Llaves
-  bool SW_0=false;
-  // Analogico
-  int variable_0=0;
-  int variable_1=0;
-  int variable_2=0;
-  int variable_3=0;
-  ////////////////////
-  int distancia_act_1;
-  int distancia_act_2;
-  int distancia_act_3;
-  int dist_mov; // distancia que se debe mover el motor en realidad
+const int ledPin = 3;
 
-//-------------------//
-void get_json(EthernetClient client);
-void post_json(char instrucciones[const_instruc], EthernetClient client);
+void setup () {
+ 
+  Serial.begin(115200);
+  Serial.println("Test del Modulo  ENC28J60");
+  ether.hisport=22;
+ 
+  if (!ether.begin(sizeof Ethernet::buffer, mymac, 53))
+    Serial.println( "No se ha podido acceder a la controlador Ethernet");
+ else
+   Serial.println("Controlador Ethernet inicializado");
+ 
+  if (!ether.staticSetup(myip))
+    Serial.println("No se pudo establecer la dirección IP");
 
-//////////// declaración de salidas ///////////////////
-//---------------- Motores --------------------------//
-
-void setup() {
-  // Initialize Arduino server parameters
-  uint8_t mac[6] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-  IPAddress myIP(172,20,5,140);
-  // Initialize serial 0 port
-  Serial.begin(9600);
-  while (!Serial) continue;
-  // Initialize Ethernet libary
-  Ethernet.begin(mac,myIP);  
-  // Start to listen
-  server.begin();
-  Serial.println(F("Server is ready."));
-  Serial.print(F("Please connect to http://"));
-  Serial.println(Ethernet.localIP());
-}
-
-void loop() 
-{
-  //////////// Strings de comunicación /////////////
-  char status[const_status] = {0};
-  char instrucciones[const_instruc] = {0};
-  // Wait for an incomming connection
-  EthernetClient client = server.available(); 
-  // Do we have a client?
-  if (!client) return;
   Serial.println();
-  Serial.println(F("New client"));
-  // Read the request (we ignore the content in this example)
-  while (client.available()) 
-  {
-    client.readBytesUntil('\r', status, sizeof(status));
-    Serial.println("status:");
-    Serial.println(status);
-//obtengo las instrucciones del formato json
-    strncpy(instrucciones,&status[15],(sizeof(status)-15));
-
-    //------ GET ----- //
-    if (strstr(status, "GET / HTTP/1.1") != NULL) 
-    {
-      get_json(client);
-    }
-  //------- POST -----//      
-    if (strstr(status, "POST / HTTP/1.1") !=NULL) 
-    {
-      post_json(instrucciones, client);
-    }
-  }
-}
-
-void get_json(EthernetClient client)
-{
-  StaticJsonDocument<256> doc;     
-  JsonArray Estado = doc.createNestedArray("Estado");
-  Estado.add(num_Lab);
-  Estado.add(subLab);
-  Estado.add(iniLab);
-
-  JsonArray Llaves = doc.createNestedArray("Llaves");
-  Llaves.add(SW_0);
-
-  JsonArray Analogico = doc.createNestedArray("Analogico");
-  Analogico.add(variable_0);
-  Analogico.add(variable_1);
-  Analogico.add(variable_2);
-  Analogico.add(variable_3);
-
-  Serial.print(F("Sending: "));
-  serializeJson(doc, Serial);
-  Serial.println();
-// Write response headers
-  client.println(F("HTTP/1.0 200 OK"));
-  client.println(F("Content-Type: application/json"));
-// client.println(F("Connection: close"));
-  client.print(F("Content-Length: "));
-  client.println(measureJsonPretty(doc));
-  client.println(); 
-// Write JSON document
-  serializeJsonPretty(doc, client);
-// Disconnect
- client.stop();
-}
-
-void post_json(char instrucciones[const_instruc], EthernetClient client)
-{
-  Serial.println("Solicitud de escritura recibida");
-  client.println(F("HTTP/1.1 200 OK"));
-  client.println();
-  StaticJsonDocument<256> doc;
-  // Deserializo
-  DeserializationError error = deserializeJson(doc, instrucciones);
   
-  if (error) 
-  {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.f_str());
-    return;
-  }
-  
-  JsonArray Estado = doc["Estado"];
-  num_Lab = Estado[0]; // 0 [Sist Dig], 1 [Sist Control], 2[Telecomunicaciones], 3[Fisica]
-  subLab = Estado[1]; // true[SubLab 1], false [SubLab 2]
-  iniLab = Estado[2]; // true[Inicia Experimento], false[Finaliza Experimento]
-
-  JsonArray Llaves = doc["Llaves"];
-  SW_0 = Llaves[0]; // Para habilitar Diafragma
-
-  JsonArray Analogico = doc["Analogico"];
-  variable_0 = Analogico[0]; // 
-  variable_1 = Analogico[1]; // 
-  variable_2 = Analogico[2]; // 
-  variable_3 = Analogico[3]; // 
-
-  if(num_Lab==3)
-  {
-    if (subLab and iniLab)
-    {
-      Serial.println("Sub - Laboratorio: Lentes convergentes"); 
-    }
-    else if (!subLab and iniLab)
-    {
-      Serial.println("Sub - Laboratorio: Lentes Divergentes");  
-    }
-    else
-    {
-      Serial.println("Laboratorio Parado");    
-    }     
-  }
-  else
-  {
-    Serial.println("Laboratorio incorrecto");    
-  }
-  // Disconnect
-//      client.stop();
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
 }
 
+static word homePage() {
+  
+ BufferFiller bfill = ether.tcpOffset();
+ bfill.emit_p(PSTR("HTTP/1.1 200 OK\r\n"   
+//      "Content-Type: text/htmlrnPragma: no-cachernRefresh: 5\r\n\r\n"
+//      "<html><head><title>Naylamp Mechatronics</title></head>"
+//      "<body>"
+//      "<div style='text-align:center;'>"
+//      "<h1>Test del Módulo  ENC28J60</h1>"      
+//      "Tiempo transcurrido : $L segundos"
+//      "<br /><br />Estado del LED: $S<br />"      
+//      "<a href=\"/?status=ON\"><input type=\"button\" value=\"ON\"></a>"
+//      "<a href=\"/?status=OFF\"><input type=\"button\" value=\"OFF\"></a>"
+//      "<br /><br />Potenciómetro: $D (resolución de 1024)"
+//      "<br /><br />"
+//      "<a href='https://naylampmechatronics.com/'>naylampmechatronics.com</a>"
+//      "</body></html>" 
+      ),millis()/1000,analogRead(0));
+     
+  return bfill.position();
+}
+  
+void loop() {
+ 
+  word len = ether.packetReceive();
+  word pos = ether.packetLoop(len);
+  
+  if(pos) {
+    
+    if(strstr((char *)Ethernet::buffer + pos, "GET / HTTP/1.1") != 0) {
+      Serial.println("Comando ON recivido");
+    }
+
+    if(strstr((char *)Ethernet::buffer + pos, "POST / HTTP/1.1") != 0) {
+      Serial.println("Comando OFF recivido");
+      bandera=1;
+      ether.httpServerReply(homePage()); // se envia OK
+    }        
+
+    if(bandera==1)
+    {
+      delay(2000);
+      digitalWrite(ledPin, HIGH);
+      delay(500);
+      digitalWrite(ledPin, LOW);
+      delay(500);
+      digitalWrite(ledPin, HIGH);
+      delay(500);
+      digitalWrite(ledPin, LOW); 
+      delay(10000);
+      digitalWrite(ledPin, HIGH);
+      delay(500);
+      digitalWrite(ledPin, LOW);
+      delay(500);
+      digitalWrite(ledPin, HIGH);
+      delay(500);
+      digitalWrite(ledPin, LOW);
+      bandera=0;
+    }
+  }
+    
+}
