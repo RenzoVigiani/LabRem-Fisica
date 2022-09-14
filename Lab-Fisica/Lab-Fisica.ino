3711fd04128ed81b9dcee4ebab936500dd9381dc
RenzoVigiani <vigiani.renzo.l@gmail.com>
2022-09-14 08:57:02 -0300

    Actualización de manejo de motores Pruebas con servos

--- Lab-Fisica/Lab-Fisica.ino
+++ Lab-Fisica/Lab-Fisica.ino
@@ -25,16 +25,28 @@ uint8_t Errores = 0;
 
 //----------------------------------------------//
 //------- Defino variables globales
+//Defino pines analogicos como digitales. Para los micro swich.
+  #define swich_m1_ini 16
+  #define swich_m1_fin 17
+  #define swich_m2_ini 18
+  #define swich_m2_fin 19
+  #define swich_m3_ini 20
+  #define swich_m3_fin 21
+
 // Nombres para los pines GPIO
+//Sentidos
+  #define AntiHorario LOW
+  #define Horario HIGH
 // Motor 1
-  #define step1 6
-  #define dir1 7
+  #define step1 22
+  #define dir1 23
 // Motor 2
-  #define step2 6
-  #define dir2 7
+  #define step2 24
+  #define dir2 25
 // Motor 3
-  #define step3 6
-  #define dir3 7
+  #define step3 26
+  #define dir3 27
+
 // Leds
   #define Led_M1 10 // Indicador Motor 1
   #define Led_M2 11 // Indicador Motor 2
@@ -54,9 +66,9 @@ uint8_t Errores = 0;
 
 // VAriables de json 
 // Estado
-  int num_Lab=0; // 0 [Sist Dig], 1 [Sist Control], 2[Telecomunicaciones], 3[Fisica] 
+  int num_Lab=3; // 0 [Sist Dig], 1 [Sist Control], 2[Telecomunicaciones], 3[Fisica] 
   bool subLab=0; // 1 [SubLab 1], 0 [SubLab 2]
-  bool iniLab=1;// 1 [Inicia Experimento], 0 [Finaliza Experimento]
+  bool iniLab=0;// 1 [Inicia Experimento], 0 [Finaliza Experimento]
 // Analogico
   int Analogico_0=0; // Tipo de diafragma
   int Analogico_1=0; // Cantidad de mediciones
@@ -64,19 +76,20 @@ uint8_t Errores = 0;
   int Analogico_3=0; // Distancia 2
   int Analogico_4=0; // Distancia 3
 //--- Variables auxiliares ---//
-  int distancia_act_1; //Distancias actuales de cada motor
-  int distancia_act_2;
-  int distancia_act_3;
+  int distancia_act_1=10; //Distancias actuales de cada motor
+  int distancia_act_2=10;
+  int distancia_act_3=10;
   int dist_mov; // distancia que se debe mover el motor en realidad
   bool bandera_vueltas=0;// sirve para hacer el conteo de vueltas. Cuando termina se activa.
   bool bandera_rep=0; // bandera para limitar la cantidad de repeticiones de mensaje de lab incorrecto 
   bool bandera_fin_m1=0; // bandera para determinar fin de mov de motor 1
   bool bandera_fin_m2=0; // bandera para determinar fin de mov de motor 2
   bool bandera_fin_m3=0; // bandera para determinar fin de mov de motor 3
-  bool sentido=0;
+  bool bandera_cero=false;    // indica que ya se puso a cero el motor.
+  int stepDelay = 5; //delay de cada paso del motor
+  int conta_pasos=0; // contador de pasos para realizar una vuelta.
+  bool sentido=0; // indica el sentido de giro.
 // Nombres de variables auxiliares
-  int v_time = 0;
-  const int ledPin = 3;
   const int limite_inferior_riel=0; // Indica el minimo valor que puede tomar distancia
   const int limite_superior_riel=900; // indica el minimo valor que puede tomar distancia.
 //----------------------------------------------//
@@ -96,20 +109,26 @@ void setup() {
 // Motor 1
   pinMode(step1, OUTPUT);
   pinMode(dir1, OUTPUT);
+  pinMode(swich_m1_ini,INPUT_PULLUP);
+  pinMode(swich_m1_fin,INPUT_PULLUP);
 // Motor 2
   pinMode(step2, OUTPUT);
   pinMode(dir2, OUTPUT);
+  pinMode(swich_m2_ini,INPUT_PULLUP);
+  pinMode(swich_m2_fin,INPUT_PULLUP);
 // Motor 3
   pinMode(step3, OUTPUT);
   pinMode(dir3, OUTPUT);
-// Leds
+  pinMode(swich_m3_ini,INPUT_PULLUP);
+  pinMode(swich_m3_fin,INPUT_PULLUP);
+  // Leds
   pinMode(Led_M1, OUTPUT); // Led Motor 1
   pinMode(Led_M2, OUTPUT); // Led Motor 2
   pinMode(Led_M3, OUTPUT); // Led Motor 3
   pinMode(Led_aux, OUTPUT);// Led Aux
   pinMode(Led_S1, OUTPUT); // Led Servo 1
   pinMode(Led_S2, OUTPUT); // Led Servo 2
-  pinMode(ledPin,OUTPUT);
+
 // Foco (LED)
   pinMode(Foco_pin, OUTPUT);
 // Servos
@@ -130,9 +149,7 @@ void loop() {
       client.readBytesUntil('\r', Mensaje_recibido, sizeof(Mensaje_recibido)); // Tomo el mensaje recibido.
       strncpy(valores_recibidos,&Mensaje_recibido[15],(sizeof(Mensaje_recibido)-15)); 
       Serial.print("Mensaje Recibido: ");
-      Serial.println(Mensaje_recibido);   
-//      Serial.print("Json_Recibido: ");
-//      Serial.println(valores_recibidos);   
+      Serial.println(Mensaje_recibido);   //      Serial.print("Json_Recibido: "); //      Serial.println(valores_recibidos);   
       //------ GET ----- //
       if (strstr(Mensaje_recibido, "GET /HTTP/1.1") != NULL) { // Compruebo si llega un GET, respondo valores actuales
         StaticJsonDocument<256> doc;     
@@ -140,32 +157,24 @@ void loop() {
         Estado.add(num_Lab);
         Estado.add(subLab);
         Estado.add(iniLab);
-
         JsonArray Analogicos = doc.createNestedArray("Analogicos");
         Analogicos.add(Analogico_0);
         Analogicos.add(Analogico_1);
         Analogicos.add(distancia_act_1);
         Analogicos.add(distancia_act_2);
         Analogicos.add(distancia_act_3);
-
         JsonArray Error = doc.createNestedArray("Error");  
         Error.add(Errores);
-
         Serial.print(F("Sending: "));
         serializeJson(doc, Serial);
         Serial.println();
-  // Write response headers
-        client.println(F("HTTP/1.0 200 OK"));
+        client.println(F("HTTP/1.0 200 OK"));   // Write response headers
         client.println(F("Content-Type: application/json"));
-  // client.println(F("Connection: close"));
-        client.print(F("Content-Length: "));
+        client.print(F("Content-Length: "));  // client.println(F("Connection: close"));
         client.println(measureJsonPretty(doc));
         client.println();
-  // Write JSON document
-        serializeJsonPretty(doc, client);
-  // Disconnect
-        client.println(F("Get terminado"));
-  //      client.stop();
+        serializeJsonPretty(doc, client);  // Write JSON document
+        client.println(F("Get terminado"));  // Disconnect  //      client.stop();
       }
       //------- POST -----//      
         if (strstr(Mensaje_recibido, "POST /HTTP/1.1") !=NULL) { // Compruebo si llega un POST y respondo, habilito banderas.
@@ -181,12 +190,10 @@ void loop() {
           Serial.println(error.f_str());
           return;
         }
-
         JsonArray Estado = doc["Estado"];
         num_Lab = Estado[0]; // 0 [Sist Dig], 1 [Sist Control], 2[Telecomunicaciones], 3[Fisica]
         subLab = Estado[1]; // 1 [SubLab 1], 0 [SubLab 2]
         iniLab = Estado[2]; // 1 [Inicia Experimento], 0 [Finaliza Experimento]
-
         if(num_Lab==3){ // Control de numero de lab.
           JsonArray Analogico = doc["Analogico"];
           Analogico_0 = Analogico[0];
@@ -199,27 +206,33 @@ void loop() {
     }
   }
   else{ // Si no está el cliente enviando algo, sigo haciendo lo que corresponde.
-      Control();
+    Control();
   }
 }
+
 /**
  * @brief Funcion utilizada para el control de laboratorios. "Se analiza las variables de entrada y se ejecuta lo necesario"
  * 
  */
 void Control(){
   if(num_Lab==3 and bandera_vueltas==0){ // Control de numero de lab.
-    if (subLab and iniLab){
-      Serial.println("Sub - Laboratorio: Lentes convergentes"); 
-      Convergentes(Analogico_0, Analogico_1, Analogico_2,Analogico_3);
-    }
-    else if (!subLab and iniLab){
-      Serial.println("Sub - Laboratorio: Lentes Divergentes");  
-      Divergentes(Analogico_0, Analogico_1, Analogico_2, Analogico_3,Analogico_4);
-    }
-    else{
-      if(bandera_rep==0){
-        Serial.println("Laboratorio Parado");
-        bandera_rep=1;
+    if(!bandera_cero){ 
+      busco_cero();
+      if(distancia_act_1==0) {bandera_cero=true;}
+    }else{
+      if (subLab and iniLab){
+        Serial.println("Sub - Laboratorio: Lentes convergentes"); 
+        Convergentes(Analogico_0, Analogico_1, Analogico_2,Analogico_3);
+      }
+      else if (!subLab and iniLab){
+        Serial.println("Sub - Laboratorio: Lentes Divergentes");  
+        Divergentes(Analogico_0, Analogico_1, Analogico_2, Analogico_3,Analogico_4);
+      }
+      else{
+        if(bandera_rep==0){
+          Serial.println("Laboratorio Parado");
+          bandera_rep=1;
+        }
       }
     }
   }
@@ -230,25 +243,7 @@ void Control(){
       Errores=4;
     }
   }
-//  hacer();
-//  delay(500);
 }
-/*
-void hacer(){
-  if(v_time == 2){
-    if(digitalRead(ledPin)==0){
-        digitalWrite(ledPin, 1);
-    }
-    else if(digitalRead(ledPin)==1){
-        digitalWrite(ledPin, 0);
-    }
-    v_time = 0;
-  }
-  else{
-    v_time++;
-  }
-}
-*/
 
 /**
  * @brief Funcion utilizada para la ejecución del laboratorio de lentes convergentes
@@ -260,6 +255,7 @@ void hacer(){
  */
 void Convergentes(int diafragma, int cant_med, int distancia_fl, int distancia_lp){
   digitalWrite(Foco_pin, HIGH); // Enciendo FOCO
+  servo_lente.write(0);
   switch (diafragma){ // Posicion del diafragma
     case 0:
       servo_diafragma.write(0);// Desplazamos a la posición 0º
@@ -299,6 +295,7 @@ void Convergentes(int diafragma, int cant_med, int distancia_fl, int distancia_l
  */
 void Divergentes(int diafragma, int cant_med, int distancia_fl1, int distancia_l1l2, int distancia_l2p){
   Serial.println("Divergentes");
+  servo_lente.write(90);
   switch (diafragma){ // Posicion del diafragma
     case 0:
       servo_diafragma.write(0);// Desplazamos a la posición 0º
@@ -338,23 +335,23 @@ void Control_Motor(int motor, int distancia){
     case 1:
       sentido = control_distancia(distancia_act_1, distancia);
       if(dist_mov==0) {bandera_fin_m1 = 1; }//Serial.println("bandera Fin M1");}
-      Serial.println("Distancia requerida Motor 1: " + (String)distancia);
-      Serial.println("Distancia actual Motor 1: " + (String)distancia_act_1);
+//      Serial.println("Distancia requerida Motor 1: " + (String)distancia);
+//      Serial.println("Distancia actual Motor 1: " + (String)distancia_act_1);
       distancia_act_1 = controlDriver(dist_mov,distancia_act_1,sentido,step1,dir1);
       
       break;
     case 2:
       sentido = control_distancia(distancia_act_2, distancia);
       if(dist_mov==0) {bandera_fin_m2 = 1;}//Serial.println("bandera Fin M2");}
-      Serial.println("Distancia requerida Motor 2: " + (String)distancia);
-      Serial.println("Distancia actual Motor 2: " + (String)distancia_act_2);
+//      Serial.println("Distancia requerida Motor 2: " + (String)distancia);
+//      Serial.println("Distancia actual Motor 2: " + (String)distancia_act_2);
       distancia_act_2 = controlDriver(dist_mov,distancia_act_2,sentido,step2,dir2);
       break;
     case 3:
       sentido = control_distancia(distancia_act_3, distancia);
       if(dist_mov==0) {bandera_fin_m3 = 1;}//Serial.println("bandera Fin M3");}
-      Serial.println("Distancia requerida Motor 3: " + (String)distancia);
-      Serial.println("Distancia actual Motor 3: " + (String)distancia_act_3);
+//      Serial.println("Distancia requerida Motor 3: " + (String)distancia);
+//      Serial.println("Distancia actual Motor 3: " + (String)distancia_act_3);
       distancia_act_3 = controlDriver(dist_mov,distancia_act_3,sentido,step3,dir3);
       break;
     default:
@@ -397,21 +394,50 @@ bool control_distancia(int distancia_act, int distancia){
  */
 int controlDriver(int dist_mov, int aux_dist_actual, bool sentido,int step, int dir){ 
   const int factor_vueltas = 200; //200 - una vuelta entera. (0.7 mm de paso)
-  int vueltas= dist_mov * factor_vueltas;
-  if((vueltas == 0)){
-    digitalWrite(step,0);
-    Serial.println("Vueltas=0");
+  int pasos= dist_mov * factor_vueltas;
+  if((pasos == 0)){
+    digitalWrite(step,LOW);
   }
-  if(vueltas>0){
-    digitalWrite(dir,sentido);
+  if(pasos>0){
+    mover_motor(dir, step, sentido, 100);  
+    conta_pasos++;
     if(sentido){// giro positivo
-      if(digitalRead(step)==0) digitalWrite(step,1);
-      if( (vueltas % factor_vueltas) == 0) aux_dist_actual++; // sumo cuenta distancia
+      if(conta_pasos == 200){ aux_dist_actual++; conta_pasos=0;}// sumo cuenta distancia
     }else{      //giro negativo
-      if(digitalRead(step)==1) digitalWrite(step,0);
-      if( (vueltas % factor_vueltas) == 0) aux_dist_actual--;// resto cuenta distancia
+      if(conta_pasos == 200){ aux_dist_actual--; conta_pasos=0;}// resto cuenta distancia
     }
-    delay(2);
   }
   return aux_dist_actual;
+}
+
+void busco_cero(){
+  if(!digitalRead(swich_m1_ini)){ 
+    distancia_act_1 =0;
+    Serial.println("Motor 1 puesto a cero");
+  }else{
+    mover_motor(dir1,step1,AntiHorario,50);
+  }
+  if(digitalRead(swich_m1_ini)==LOW){ 
+    distancia_act_2 =0; 
+    Serial.println("Motor 2 puesto a cero");
+  }else{
+    mover_motor(dir2,step2,AntiHorario,20);  
+    Serial.println("Motor 2 girando");
+  }
+  if(digitalRead(swich_m1_ini)==LOW){ 
+    distancia_act_3 =0; 
+    Serial.println("Motor 3 puesto a cero");
+  }else{ 
+    mover_motor(dir3,step3,AntiHorario,20);  
+    Serial.println("Motor 3 girando");
+  }
+}
+
+void mover_motor(int dir, int step, int sentido, int velocidad){
+//  int stepDelay = (10 + 500 -(velocidad*5)) ; // velocidad va de 1 a 100 %
+  digitalWrite(dir,sentido);  
+  digitalWrite(step, HIGH);
+  delay(stepDelay);
+  digitalWrite(step, LOW);
+  delay(stepDelay);
 }
